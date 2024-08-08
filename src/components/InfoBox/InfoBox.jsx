384c56ae4f797f4eda0f13dac43a03b6bf595d04
Sobdev <71671270+Sobdev@users.noreply.github.com>
2024-08-08 20:32:31 +0200

    Full Bootstrap AboutUs page cleaned also InfoBox component finished and applied

--- src/components/InfoBox/InfoBox.jsx
+++ src/components/InfoBox/InfoBox.jsx
@@ -1,15 +1,29 @@
 import PropTypes from 'prop-types';
+import { Card } from 'react-bootstrap';
+import './InfoBox.css';
 
-export const InfoBox = ({ title, description }) => {
+export const InfoBox = ({ title, image, name, subtle, subtitle, description, info, infoCentered }) => {
     return (
-        <div className="info-box mb-5">
+        <div className="info-box">
             <h1 className='h1-gradient text-center'>{title}</h1>
-            <p className='text-center' style={{ fontStyle: 'italic' }}>{description}</p>
+            <Card.Img src={image} className="mb-3" />
+            <Card.Title className="mb-2 text-center">{name}</Card.Title>
+            <Card.Subtitle className="mb-2 text-center text-muted">{subtle}</Card.Subtitle>
+            <Card.Subtitle className="fs-4 mb-2 text-center text-uppercase ">{subtitle}</Card.Subtitle>
+            <Card.Text className="description mb-2 text-center fst-italic">{description}</Card.Text>
+            <Card.Text className="mb-2 ps-5">{info}</Card.Text>
+            <Card.Text className="ps-2 pe-2 text-center">{infoCentered}</Card.Text>
         </div>
     );
 };
 
 InfoBox.propTypes = {
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired
+    title: PropTypes.string,
+    image: PropTypes.string,
+    name: PropTypes.string,
+    subtle: PropTypes.string,
+    subtitle: PropTypes.string,
+    description: PropTypes.string,
+    info: PropTypes.string,
+    infoCentered: PropTypes.string
 };
--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,52 +1,48 @@
-import { Container, Card, Row, Col } from 'react-bootstrap';
-import './AboutUs.css';
 import Confetti from "react-confetti"
 import { InfoBox } from '../../components/InfoBox/InfoBox';
-
-const teamMembers = [
-    {
-        name: "Cristina Colomoiets",
-        role: "Git merge Queen",
-        image: "https://res.cloudinary.com/drpdy7tju/image/upload/v1717609633/1653902628078_qqcjnx.jpg",
-        description: "Cristina is a talented frontend developer passionate about crafting stunning web experiences."
-    },
-    {
-        name: "Samuel Pérez Morcillo",
-        role: "Monster DB",
-        image: "https://res.cloudinary.com/drpdy7tju/image/upload/v1717609633/1716231063988_jqcd31.jpg",
-        description: "Samuel is our backend wizard, ensuring our server-side logic runs smoothly and efficiently."
-    },
-    {
-        name: "Adrian Sobota Matuszak",
-        role: "React Monster Bootstrap",
-        image: "https://res.cloudinary.com/drpdy7tju/image/upload/v1717609947/Sin_ti%CC%81tulo_500_x_500_px_300_x_300_px_webpid.png",
-        description: "Adrian is a versatile full stack developer, bridging the gap between front and backend."
-    }
-];
+import { Container, Card, Row, Col } from 'react-bootstrap';
 
 const AboutUs = () => {
     return (
-        <Container className="about-us-container">
+        <Container className='mt-5 mb-5'>
             <Confetti />
-            <InfoBox title="Hey its us! 👋" description="This project was a collaborative effort to create a modern web application using technologies such as React, Bootstrap, Node.js, Express and MongoDB. Our goal was to build a seamless and user-friendly experience for our users using all our knowledge adquired during our bootcamp in IronHack Madrid." />
-            <Row className="justify-content-center">
-                {teamMembers.map((member, index) => (
-                    <Col md={4} key={index} className="mb-4">
-                        <Card className="team-member-card text-center">
-                            <Card.Img variant="top" src={member.image} className="team-member-image mx-auto" />
-                            <Card.Body>
-                                <Card.Title>{member.name}</Card.Title>
-                                <Card.Subtitle className="mb-2 text-muted">{member.role}</Card.Subtitle>
-                                <Card.Text>{member.description}</Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                ))}
+            <InfoBox
+                title="Hey its us! 👋"
+                description="This project was a collaborative effort to create a modern web application using technologies such as React, Bootstrap, Node.js, Express and MongoDB. Our goal was to build a seamless and user-friendly experience for our users using all our knowledge adquired during our bootcamp in IronHack Madrid."
+            />
+            <Row>
+                <Col md={4}>
+                    <Card className="mt-2 pb-5">
+                        <InfoBox
+                            image="https://res.cloudinary.com/drpdy7tju/image/upload/v1717609633/1653902628078_qqcjnx.jpg"
+                            name="Cristina Colomoiets"
+                            subtle="Git merge Queen"
+                            infoCentered="Cristina is a talented frontend developer passionate about crafting stunning webs."
+                        />
+                    </Card>
+                </Col>
+                <Col md={4}>
+                    <Card className="mt-2 pb-5">
+                        <InfoBox
+                            image="https://res.cloudinary.com/drpdy7tju/image/upload/v1717609633/1716231063988_jqcd31.jpg"
+                            name="Samuel Pérez Morcillo"
+                            subtle="Monster DB"
+                            infoCentered="Samuel is our backend wizard, ensuring our server-side logic runs smoothly and efficiently."
+                        />
+                    </Card>
+                </Col>
+                <Col md={4}>
+                    <Card className="mt-2 pb-5">
+                        <InfoBox
+                            image="https://res.cloudinary.com/drpdy7tju/image/upload/v1717609947/Sin_ti%CC%81tulo_500_x_500_px_300_x_300_px_webpid.png"
+                            name="Adrian Sobota Matuszak"
+                            subtle="React Monster Bootstrap"
+                            infoCentered="Adrian is a versatile full stack developer, bridging the gap between front and backend."
+                        />
+                    </Card>
+                </Col>
             </Row>
         </Container>
-
-
-
     );
 };
 
